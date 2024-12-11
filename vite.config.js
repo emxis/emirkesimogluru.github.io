704fcdf87095ff203d826770bb130001fbff544e
emxis <sessiz.oyuncu.923@gmail.com>
2024-12-11 13:35:16 +0300

    Deploy project to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // GitHub Pages kök URL'yi ayarlar
+  base: '/', // GitHub Pages için gerekli olan base ayarı
 });
